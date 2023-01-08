15f46a7e007a44d566454db8ed95bead74cc2bac
Siyun Lee <siyun1221@gmail.com>
2023-01-08 21:40:32 +0900

    Fix : Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client

--- pages/api/contacts.js
+++ pages/api/contacts.js
@@ -1,8 +1,6 @@
 import client from '../../lib/sanity/client'
 
 export default async function handler(req, res) {
-  const response = await client.fetch('*[_type == "contact"]')
-  res.status(200).json(response)
   switch (req.method) {
     case 'DELETE':
       await client.delete(req.body).then(res => {
